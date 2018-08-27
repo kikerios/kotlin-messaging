b3a31c2a41b38dccb85438192269785e6b147151
kikerios <web.lrios@gmail.com>
2018-08-27 09:08:43 -0600

    remove toast from splash

--- feature/src/main/java/kikerios/me/kotlinmessaging/feature/LaunchActivity.kt
+++ feature/src/main/java/kikerios/me/kotlinmessaging/feature/LaunchActivity.kt
@@ -14,8 +14,6 @@ class LaunchActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch)
 
-        Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show()
-
         Run.after(2500, { // temp delay
             if (getUser() == null) {
                 ActivityController.goToLogin(this)
